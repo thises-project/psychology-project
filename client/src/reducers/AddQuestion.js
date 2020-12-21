66e92aee9486f77d95a8266c283275e3a8ee4d46
nourmouazen <nh.yu.mo@hotmail.com>
2020-12-21 15:03:26 +0200

    ask Questions backend

--- client/src/reducers/AddQuestion.js
+++ client/src/reducers/AddQuestion.js
@@ -2,14 +2,14 @@
 // // import { FETCH_ALL, CREATE, UPDATE}
 
 
-// export default ( AddQuestion = [], action) => {
-//     switch (action.type){
-//         case 'FETCH_ALL':
-//             return AddQuestion;
-//         case 'CREATE':
-//             return AddQuestion;
-//         default:
-//             return AddQuestion;
-//     }
-// }
+export default ( AddQuestion = [], action) => {
+    switch (action.type){
+        case 'FETCH_ALL':
+            return AddQuestion;
+        case 'CREATE':
+            return AddQuestion;
+        default:
+            return AddQuestion;
+    }
+}
 // // export default AskQuestion;
