63f9ae07a15a13756323866f7b9ebcd557a3ce57
nourmouazen <nh.yu.mo@hotmail.com>
2020-12-21 15:25:06 +0200

    ask Questions FE

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -4,3 +4,5 @@ const url = 'http://localhost:5000';
 
 // export const fetchPosts = () => axios.get(url);
 export  const Adduser = (user) => axios.post(url+'/users/createUser', user);
+
+export const AddQuestions = (question) => axios.post(url+'/questions/createQuestion',question);
--- client/src/reducers/AddQuestion.js
+++ client/src/reducers/AddQuestion.js
@@ -2,14 +2,12 @@
 // // import { FETCH_ALL, CREATE, UPDATE}
 
 
-export default ( AddQuestion = [], action) => {
-    switch (action.type){
-        case 'FETCH_ALL':
-            return AddQuestion;
-        case 'CREATE':
-            return AddQuestion;
-        default:
-            return AddQuestion;
-    }
-}
+// export default ( AddQuestion = [], action) => {
+//     switch (action.type){
+//         case 'AddQuestions':
+//             return [AddQuestion, action.];
+//         default:
+//             return AddQuestion;
+//     }
+// }
 // // export default AskQuestion;
