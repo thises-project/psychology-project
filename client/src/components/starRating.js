4dc62ea155a7ae3e6bb0b9b2b703ff2bc17d7cf9
farahishaqat <ishaqatfarah@gmail.com>
2020-12-28 14:13:30 +0200

    starRating-Fronend

---
+++ client/src/components/starRating.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { FaStar } from "react-icons/fa";
+
+const StarRating = () => {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+
+  return (
+    <div>
+      {[...Array(5)].map((star, i) => {
+        const ratingValue = i + 1;
+        return (
+          <label>
+            <input
+              type="radio"
+              name="rating"
+              value={ratingValue}
+              onClick={() => setRating(ratingValue)}
+            />
+            <FaStar
+              className="star"
+              color={ratingValue <= (hover || rating) ? "#ffc107" : "#e4e5e9"}
+              size={20}
+              onMouseEnter={() => setHover(ratingValue)}
+              onMouseLeave={() => setHover(0)}
+            />
+          </label>
+        );
+      })}
+      <p> {rating} out of 5 !</p>
+    </div>
+  );
+};
+
+export default StarRating;
