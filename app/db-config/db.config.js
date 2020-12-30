2f8b899412e5060f52895d9ac31019124b132bab
farahishaqat <ishaqatfarah@gmail.com>
2020-12-30 21:11:31 +0200

    deployment after keys

--- app/db-config/db.config.js
+++ app/db-config/db.config.js
@@ -1,7 +1,17 @@
-// module.exports={
-//     HOST:"us-cdbr-east-02.cleardb.com",
-//     USER:"b0633d23088e42",
-//     PASSWORD:"a0a5e72a",
-//     DB:"heroku_f537f4068a5e975"
-// }
+const HOST_NAME = require('../../config/keys')
+const USER_NAME = require('../../config/keys')
+const PASS_WORD = require('../../config/keys')
+const DATA_BASE = require('../../config/keys')
+
+module.exports={
+    HOST:HOST_NAME,
+    USER:USER_NAME,
+    PASSWORD:PASS_WORD,
+    DB:DATA_BASE,
+    connectionLimit:50,
+    queueLimit:50,
+    waitForConnection: true
+
+
+}
 
